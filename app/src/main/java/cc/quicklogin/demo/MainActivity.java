f5d6098872990da5f22313cba3ca6fe340414a6f
runqingself <catdoggame@163.com>
2021-10-18 02:04:20 +0800

    sdk 1.0.15

--- app/src/main/java/cc/quicklogin/demo/MainActivity.java
+++ app/src/main/java/cc/quicklogin/demo/MainActivity.java
@@ -7,8 +7,9 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import cc.quicklogin.sdk.LoginHelper;
-import cc.quicklogin.sdk.open.TokenInfo;
-import cc.quicklogin.sdk.open.TokenListener;
+import cc.quicklogin.sdk.open.InitResultListener;
+import cc.quicklogin.sdk.open.LoginInfo;
+import cc.quicklogin.sdk.open.LoginResultListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,33 +28,30 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 final long startTime = System.currentTimeMillis();
 
-                final LoginHelper loginHelper = LoginHelper.getInstance(getApplicationContext(), "bf49d4a3f4ff7e0aefe1efe97c57729d");
-                loginHelper.setDebug();
-                loginHelper.getToken(new TokenListener() {
+                LoginHelper.init(getApplicationContext(), "bf49d4a3f4ff7e0aefe1efe97c57729d", new InitResultListener() {
                     @Override
-                    public void onTokenSuccess(final TokenInfo tokenInfo) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                resp.setText("resultCode:" + tokenInfo.getResultCode() + ",msg:" + tokenInfo.getMsg() + ",operatorType:" + tokenInfo.getOperatorType() + ",accessCode:" + tokenInfo.getAccessCode() + ",traceId:" + tokenInfo.getTraceId() + ",mobile:" + tokenInfo.getMobile() + "\n 耗时：" + ((System.currentTimeMillis() - startTime)) + "毫秒");
+                    public void onComplete(boolean result, String msg) {
+                        if (result) {
+                            LoginHelper.getInstance().setDebug();
+                            LoginHelper.getInstance().getAccessCode(new LoginResultListener() {
+                                @Override
+                                public void onComplete(LoginInfo loginInfo) {
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            resp.setText("resultCode:" + loginInfo.getResultCode() + ",msg:" + loginInfo.getMsg() + ",operatorType:" + loginInfo.getOperatorType() + ",accessCode:" + loginInfo.getAccessCode() + ",traceId:" + loginInfo.getTraceId() + ",mobile:" + loginInfo.getMobile() + ",authCode:" + loginInfo.getAuthCode() + "\n 耗时：" + ((System.currentTimeMillis() - startTime)) + "毫秒");
+                                        }
+                                    });
+                                    System.out.println(loginInfo.toString());
+                                }
+                            }, 5000);
+                        } else {
+                            System.out.println("失败原因：" + msg);
+                        }
 
-                            }
-                        });
-                        System.out.println(tokenInfo.toString());
-                        loginHelper.destroy();
                     }
+                });
 
-                    @Override
-                    public void onTokenFail(final TokenInfo tokenInfo) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                resp.setText("code=" + tokenInfo.getResultCode() + ",msg=" + tokenInfo.getMsg());
-                            }
-                        });
-                        System.out.println(tokenInfo.toString());
-                    }
-                }, 5000);
 
             }
         });
